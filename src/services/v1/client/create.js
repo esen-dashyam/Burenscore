5aa25220d95bc4527cdc112359683520f1637065
blackcomedy0223 <blackcomedy0223@gmail.com>
2022-03-16 14:57:29 +0800

    a

--- src/services/v1/client/create.js
+++ src/services/v1/client/create.js
@@ -7,19 +7,19 @@ import randomstring from "randomstring";
 import Joi from "joi";
 
 const schema = Joi.object({
-  g_customer_id: Joi.string().required(),
-  is_active    : Joi.boolean()
+  client_id: Joi.string().required(),
+  is_active: Joi.boolean()
 });
 
 export default logic(schema, async (data, session) => {
   const {
-    g_customer_id,
+    client_id,
     is_active,
   } = data;
 
   let secretKey = randomstring.generate(50);
 
-  let client = await db.find(db.Client, { g_customer_id }, session);
+  let client = await db.find(db.Client, { client_id: client_id }, session);
   if (client) throw new ValidationError(ERRORS.ALREADY_REGISTERED);
 
 
