dcf90a4def74b3df481dfa167fbe3fb1a8b92244
gunbileg.lkh <gunbileg.lkh@gmail.com>
2022-03-16 17:12:28 +0800

    aaa

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -23,8 +23,7 @@ export default async (req, res, next) => {
     try {
       decoded = jwt.verify(token, config.jwt_api_secret);
     } catch (err) {
-      throw err;
-      // throw new UnauthorizedError(ERRORS.NO_CREDENTIALS);
+      throw new UnauthorizedError(ERRORS.NO_CREDENTIALS);
     }
 
     next();
