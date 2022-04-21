45688fe079632c483349fb5d32c55161eddd7ce8
blackcomedy0223 <blackcomedy0223@gmail.com>
2022-04-21 10:04:59 +0800

    a

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,10 +2,12 @@ import ERRORS from "./ERRORS";
 import NOS_TYPES from "./NOS_TYPES";
 import CLIENT_STATUS from "./CLIENT_STATUS";
 import VALUE_CODES from "./VALUE_CODES";
+import APPENDIX_A from "./APPENDIX_A";
 
 export {
   ERRORS,
   CLIENT_STATUS,
   NOS_TYPES,
   VALUE_CODES,
+  APPENDIX_A
 };
